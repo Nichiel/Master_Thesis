0b2e0e7e3ee8d2f7a873a167186e80d6cc9845b4
Nichiel <zofia.frackowiakk@gmail.com>
2023-08-29 18:52:24 +0200

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	new file:   ProtoPNet_modified/densenet_features.py
    	new file:   ProtoPNet_modified/find_nearest.py
    	new file:   ProtoPNet_modified/global_analysis.py
    	new file:   ProtoPNet_modified/helpers.py
    	new file:   ProtoPNet_modified/img_aug.py
    	new file:   ProtoPNet_modified/local_analysis.py
    	new file:   ProtoPNet_modified/log.py
    	new file:   ProtoPNet_modified/main.py
    	new file:   ProtoPNet_modified/model.py
    	new file:   ProtoPNet_modified/preprocess.py
    	new file:   ProtoPNet_modified/prune.py
    	new file:   ProtoPNet_modified/push.py
    	new file:   ProtoPNet_modified/receptive_field.py
    	new file:   ProtoPNet_modified/resnet_features.py
    	new file:   ProtoPNet_modified/run_pruning.py
    	new file:   ProtoPNet_modified/save.py
    	new file:   ProtoPNet_modified/settings.py
    	new file:   ProtoPNet_modified/train_and_test.py
    	new file:   ProtoPNet_modified/vgg_features.py

---
+++ ProtoPNet_modified/settings.py
@@ -0,0 +1,39 @@
+base_architecture = 'vgg19'
+img_size = 224
+prototype_shape = (2000, 128, 1, 1)
+num_classes = 10
+prototype_activation_function = 'log'
+add_on_layers_type = 'regular'
+
+experiment_run = '0015'
+
+data_path = '/content/drive/Othercomputers/Mylaptop/tensorflow_Dataset_first'
+train_dir = '/content/drive/Othercomputers/Mylaptop/tensorflow_Dataset_first/train'
+test_dir = '/content/drive/Othercomputers/Mylaptop/tensorflow_Dataset_first/validation'
+train_push_dir = '/content/drive/Othercomputers/Mylaptop/tensorflow_Dataset_first/train'
+train_batch_size = 80
+test_batch_size = 100
+train_push_batch_size = 75
+
+joint_optimizer_lrs = {'features': 1e-4,
+                       'add_on_layers': 3e-3,
+                       'prototype_vectors': 3e-3}
+joint_lr_step_size = 5
+
+warm_optimizer_lrs = {'add_on_layers': 3e-3,
+                      'prototype_vectors': 3e-3}
+
+last_layer_optimizer_lr = 1e-4
+
+coefs = {
+    'crs_ent': 1,
+    'clst': 0.8,
+    'sep': -0.08,
+    'l1': 1e-4,
+}
+
+num_train_epochs = 11
+num_warm_epochs = 5
+
+push_start = 10
+push_epochs = [i for i in range(num_train_epochs) if i % 10 == 0]
